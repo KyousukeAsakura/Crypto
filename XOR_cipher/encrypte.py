b0bc54798a0d70dc818044fa72293780563e88c6
gawain <yuuki1993server@gmail.com>
2017-11-23 18:12:34 +0900

    fix

--- XOR_cipher/encrypte.py
+++ XOR_cipher/encrypte.py
@@ -1,4 +1,17 @@
 from os import urandom
 
+def genkey(N):
+    return urandom(N)
+
 if __name__ == '__main__':
-    print(urandom(8))
+    print("input messege:", end="")
+    message = input()
+
+    key = genkey(len(message))
+    print("key:", key)
+
+    print(message.encode('utf-8'))
+    cypherTex = bytes([a ^ b for a, b in zip(message.encode('utf-8'), key)])
+    print(cypherTex)
+
+#refer from https://en.wikipedia.org/wiki/XOR_cipher
